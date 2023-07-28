61be8b67d50cf247fd4b652882722eac2bdf1855
Ale Costa <alexandrercosta01@gmail.com>
2023-07-27 22:03:19 -0300

    refac: removendo url do cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb+srv://alexandrercosta01:7Rqgp1x5fQcxp8D9@cluster1.6ybkv9x.mongodb.net/`),   
+    MongooseModule.forRoot(`mongodb://localhost/nest`),   
     UsersModule,
   ],
   controllers: [],
